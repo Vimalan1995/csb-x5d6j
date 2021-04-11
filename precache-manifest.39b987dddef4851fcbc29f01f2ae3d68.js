53503f4021dd9358ff12ed03b1737c41c563f322
Code Sandbox <hello@codesandbox.io>
2021-04-11 15:18:58 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.39b987dddef4851fcbc29f01f2ae3d68.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "453bacd1a24a7cbb1184525e9f1b3c41",
+    "url": "https://vimalan1995.github.io/csb-x5d6j/index.html"
+  },
+  {
+    "revision": "d478e7f699f8a4fe0e4f",
+    "url": "https://vimalan1995.github.io/csb-x5d6j/static/js/2.fc726699.chunk.js"
+  },
+  {
+    "revision": "b22440b9bade1321172f",
+    "url": "https://vimalan1995.github.io/csb-x5d6j/static/js/main.263db125.chunk.js"
+  },
+  {
+    "revision": "9e0c79488e8045d065dd",
+    "url": "https://vimalan1995.github.io/csb-x5d6j/static/js/runtime-main.ccc7ecaa.js"
+  }
+]);
